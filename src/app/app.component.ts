7520f677fb6ee95be280e3c7ab94a2d891e1103a
KielRodriguez <kr.rdz.20@gmail.com>
2017-09-07 18:10:48 -0500

    primer componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziScuare';
+  lat:number = 19.2790452;
+  lng:number = -99.1681434;
+  negocios:any = [
+    {plan: "pagado", cercania: 1, active: true, nombre: "Donas la pasadita"},
+    {plan: "gratuito", cercania: 1, active: true, nombre: "Floreria la Gardenia"},
+    {plan: "gratuito", cercania: 3, active: false, nombre: "Veterinaria Huellitas Felices"},
+    {plan: "pagado", cercania: 1, active: true, nombre: "Floreria la Gardenia"},
+    {plan: "gratuito", cercania: 2, active: true, nombre: "Donas la pasadita"},
+    {plan: "pagado", cercania: 1, active: false, nombre: "Veterinaria Huellitas Felices"}
+  ]
+  constructor(){
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective} from './directives/resaltar.directive'
+import { ContarClicks } from './directives/contar-clicks.directive'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicks
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCULxSOKfiAvUmVTqZ1Mlxpbb5Tsk8C7lA'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
